0780bb7489f44cf387e04764019200e3051e5fe3
milovicaleksandar001 <milovicaleksandar001@gmail.com>
2023-11-16 20:40:09 +0100

    first task done

--- src/main/java/com/example/isabackend/controller/AuthController.java
+++ src/main/java/com/example/isabackend/controller/AuthController.java
@@ -35,4 +35,52 @@ public class AuthController {
         user = userService.save(user);
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
     }
+    @GetMapping(value = "/user/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Integer id) {
+        User user = userService.findOne(id);
+
+        if (user != null) {
+            // Map User entity to UserDTO if needed
+            UserDTO userDTO = new UserDTO();
+            userDTO.setEmail(user.getEmail());
+            userDTO.setRole(user.getRole());
+            userDTO.setId(user.getId());
+            userDTO.setFirstName(user.getFirstName());
+            userDTO.setLastName(user.getLastName());
+            userDTO.setPassword(user.getPassword());
+            userDTO.setCountry(user.getCountry());
+            userDTO.setCity(user.getCity());
+            userDTO.setProfession(user.getProfession());
+            userDTO.setPhoneNumber(user.getPhoneNumber());
+            userDTO.setCompanyInfo(user.getCompanyInfo());
+
+            return new ResponseEntity<>(userDTO, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PutMapping(value = "/user/{id}", consumes = "application/json")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Integer id, @RequestBody UserDTO updatedUserDTO) {
+        User existingUser = userService.findOne(id);
+
+        if (existingUser != null) {
+            // Update existingUser with values from updatedUserDTO
+            existingUser.setFirstName(updatedUserDTO.getFirstName());
+            existingUser.setLastName(updatedUserDTO.getLastName());
+            existingUser.setEmail(updatedUserDTO.getEmail());
+            existingUser.setPassword(updatedUserDTO.getPassword());
+            existingUser.setCountry(updatedUserDTO.getCountry());
+            existingUser.setCity(updatedUserDTO.getCity());
+            existingUser.setProfession(updatedUserDTO.getProfession());
+            existingUser.setPhoneNumber(updatedUserDTO.getPhoneNumber());
+            existingUser.setCompanyInfo(updatedUserDTO.getCompanyInfo());
+
+            // Save the updated user
+            existingUser = userService.save(existingUser);
+
+            return new ResponseEntity<>(updatedUserDTO, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
