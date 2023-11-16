c5b175b44a0d7acce7b63f8aca3c04a23a608fe4
z1cLa <miljanlazic@gmail.com>
2023-11-16 13:27:04 +0100

    Added needed crud functions

--- src/main/java/com/example/isabackend/controller/CompanyController.java
+++ src/main/java/com/example/isabackend/controller/CompanyController.java
@@ -1,16 +1,32 @@
 package com.example.isabackend.controller;
 
+import com.example.isabackend.model.Company;
 import com.example.isabackend.service.CompanyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/company")
 @RequiredArgsConstructor
 @CrossOrigin
 public class CompanyController {
-    @Autowired
-    private CompanyService companyService;
 
+    private final CompanyService companyService;
+
+    @GetMapping("/{id}")
+    public Company getCompanyById(@PathVariable Integer id) {
+        return companyService.findById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Company> getAllCompanies() {
+        return companyService.findAll();
+    }
+
+    @PutMapping("/update/{id}")
+    public Company updateCompany(@PathVariable Integer id, @RequestBody Company updatedCompany) {
+        return companyService.update(id, updatedCompany);
+    }
 }
--- src/main/java/com/example/isabackend/service/CompanyService.java
+++ src/main/java/com/example/isabackend/service/CompanyService.java
@@ -17,7 +17,7 @@ public class CompanyService {
 
     public Company findById(Integer id) { return companyRepository.findById(id).orElseGet(null);}
 
-    public List<Company> findAll() { return companyRepository.findAll();}
+    public List<Company> findAll() {return companyRepository.findAll();}
 
     public Company save(Company exam) {
         return companyRepository.save(exam);
@@ -25,6 +25,23 @@ public class CompanyService {
 
     public void remove(Integer id) {
         companyRepository.deleteById(id);
+    }
+
+    public Company update(Integer id, Company updatedCompany) {
+        Company existingCompany = companyRepository.findById(id).orElse(null);
+
+        if (existingCompany != null) {
+
+            existingCompany.setCompanyName(updatedCompany.getCompanyName());
+            existingCompany.setAddress(updatedCompany.getAddress());
+            existingCompany.setDescription(updatedCompany.getDescription());
+            existingCompany.setAverageGrade(updatedCompany.getAverageGrade());
+            existingCompany.setFreeAppointments(updatedCompany.getFreeAppointments());
+
+            return companyRepository.save(existingCompany);
+        }
+
+        return null;
     }
 
 }
