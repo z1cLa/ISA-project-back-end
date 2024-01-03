4f235eadcd9995daead250008f50b842d78d2279
pavke02 <mpavkovic02@gmail.com>
2024-01-03 09:59:03 +0100

    AppointmentRepository 2

--- src/main/java/com/example/isabackend/repository/AppointmentRepository.java
+++ src/main/java/com/example/isabackend/repository/AppointmentRepository.java
@@ -3,5 +3,8 @@ package com.example.isabackend.repository;
 import com.example.isabackend.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
+    List<Appointment> findByCompanyIdAndIsCompaniesAppointmentTrue(Integer companyId);
 }
