ecb881caf186c985fc00dd8faf6efe30be292408
pavke02 <mpavkovic02@gmail.com>
2024-01-03 09:45:33 +0100

    Reservation Domain

---
+++ src/main/java/com/example/isabackend/model/Reservation.java
@@ -0,0 +1,35 @@
+package com.example.isabackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_reservation")
+public class Reservation {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "appointment_id")
+    private Appointment appointment;
+
+    @ManyToOne
+    @JoinColumn(name = "equipment_id")
+    private Equipment equipment;
+}
