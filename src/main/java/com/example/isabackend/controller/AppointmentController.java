80493f0ab5e9a34ea777ddf9ab2f90ab432cc003
pavke02 <mpavkovic02@gmail.com>
2024-01-03 09:35:08 +0100

    Controller added

---
+++ src/main/java/com/example/isabackend/controller/AppointmentController.java
@@ -0,0 +1,35 @@
+package com.example.isabackend.controller;
+
+import com.example.isabackend.model.Appointment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/appointment")
+@RequiredArgsConstructor
+@CrossOrigin
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @GetMapping("/{id}")
+    public Appointment getAppointmentById(@PathVariable Integer id) {
+        return appointmentService.findById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Appointment> getAllAppointments() {
+        return appointmentService.findAll();
+    }
+    @PostMapping("/save")
+    public Appointment saveAppointment(@RequestBody Appointment appointment) {
+        return appointmentService.save(appointment);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteAppointment(@PathVariable Integer id) {
+        appointmentService.remove(id);
+    }
+
+}
