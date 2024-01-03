b8e229d54a017d1fa92e5445be3274a21e51b519
pavke02 <mpavkovic02@gmail.com>
2024-01-03 09:49:54 +0100

    ReservationDTO

---
+++ src/main/java/com/example/isabackend/dto/ReservationDTO.java
@@ -0,0 +1,21 @@
+package com.example.isabackend.dto;
+
+import com.example.isabackend.model.Appointment;
+import com.example.isabackend.model.Equipment;
+import com.example.isabackend.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationDTO {
+    private Integer id;
+    private String status;
+    private User user;
+    private Appointment appointment;
+    private Equipment equipment;
+}
